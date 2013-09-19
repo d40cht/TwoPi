6d52841ba818fe37d1ec7d58f9565169291eda64
Alex Wilson <alex.wilson@navetas.com>
2013-09-19 15:28:53 +0100

    OSM and highcharts working.

--- routeSite/src/main/webapp/static/angulartest.js
+++ routeSite/src/main/webapp/static/angulartest.js
@@ -20,6 +20,69 @@ function localStorageWatch( scope, name )
     } );
 }
 
+function buildMap( mapId )
+{
+    var mousePosition = new OpenLayers.Control.MousePosition();
+    var map = new OpenLayers.Map( mapId, {
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.PanZoomBar(),
+            new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.Attribution(),
+            mousePosition],
+        projection: new OpenLayers.Projection("EPSG:4326"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326")
+    } );
+    
+    OpenLayers.Events.prototype.includeXY = true;
+
+    // Define the map layer
+    // Here we use a predefined layer that will be kept up to date with URL changes
+    
+    layerMapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+    map.addLayer(layerMapnik);
+    layerCycleMap = new OpenLayers.Layer.OSM.CycleMap("CycleMap");
+    map.addLayer(layerCycleMap);
+    
+    var lon = -5.208;
+    var lat = 54.387;
+    var zoom = 5;
+    var lonLat = new OpenLayers.LonLat(lon, lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+    map.setCenter(lonLat, zoom);
+    
+    return map;
+}
+
+function buildElevationGraph( divId, seriesData )
+{
+    externalSeriesData = seriesData
+    $("#"+divId).highcharts({
+        chart : { type : 'line' },
+        title : { text : 'Elevation profile' },
+        xAxis : { title : { text : 'Distance' } },
+        yAxis : { title : { text : '(m)' } },
+        series : [{ showInLegend: false, name : 'elevation', type : 'area', data : seriesData }],
+        plotOptions :
+        {
+            series :
+            {
+                marker : { enabled : false },
+                point :
+                {
+                    events :
+                    {
+                        mouseOver : function()
+                        {
+                            var lonLat = new OpenLayers.LonLat(this.lon, this.lat).transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+                            moveMapCrossLinkMarker( lonLat );
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
 function RouteController($scope, $log)
 {
     $scope.routingPreferences = ["Walking", "Cycling"];
@@ -29,4 +92,8 @@ function RouteController($scope, $log)
     
     localStorageWatch( $scope, 'distance' );
     localStorageWatch( $scope, 'routingPreference' );
+    
+    //var m = buildMap( "map" );
+    
+    //$('#routeMethodsTab a:last').tab('show')
 }
